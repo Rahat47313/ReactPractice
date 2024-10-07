866e8b5d507fef2c6070411bbb4d51ce5f1c4225
Rahat47313 <rahat47313@gmail.com>
2024-10-07 16:35:45 +0600

    make TenziesApp die color toggle-able

--- src/tenzies/Die.jsx
+++ src/tenzies/Die.jsx
@@ -2,12 +2,14 @@ import PropTypes from "prop-types";
 
 export default function Die(props){
     return(
-        <div className="flex justify-center items-center box-border cursor-pointer text-[2rem] bg-white rounded-md shadow-[0px_2px_2px_rgba(0,0,0,0.15)] w-[50px] h-[50px]">
+        <div onClick={props.holdDice} className={`flex justify-center items-center box-border cursor-pointer text-[2rem] ${props.isHeld? 'bg-[#59e391]':'bg-white'} rounded-md shadow-[0px_2px_2px_rgba(0,0,0,0.15)] w-[50px] h-[50px]`}>
             <div>{props.value}</div>
         </div>
     );
 }
 
 Die.propTypes = {
+    holdDice: PropTypes.func,
+    isHeld: PropTypes.bool,
     value: PropTypes.number,
   };
--- src/tenzies/TenziesApp.jsx
+++ src/tenzies/TenziesApp.jsx
@@ -1,28 +1,50 @@
 import { useState } from "react";
+import {nanoid} from "nanoid"
 import Die from "./Die";
 
 export default function TenziesApp() {
   const [dice, setDice] = useState(allNewDice());
 
   function allNewDice() {
-    const randNumArray = [...Array(10)].fill().map(() => Math.floor(Math.random() * 6) + 1);
-    return randNumArray;
+    let newDice = [];
+
+    for (let i = 0; i < 10; i++) {
+      newDice.push({
+        id: nanoid(),
+        value: Math.floor(Math.random() * 6) + 1,
+        isHeld: false,
+      });
+    }
+
+    //const randNumArray = [...Array(10)].fill().map(() => Math.floor(Math.random() * 6) + 1);
+    return newDice;
   }
 
-  function rollDice(){
-    setDice(allNewDice())
+  const holdDice = (id) => {
+    setDice(oldDice => oldDice.map((die) => {
+        return die.id === id ? {...die, isHeld: !die.isHeld} : die
+    }))
   }
 
-  const diceElement = dice.map((die, index) => <Die key={index} value={die} />);
+//   function rollDice() {
+//     setDice(allNewDice());
+//   }
+
+  const diceElement = dice.map((die) => <Die key={die.id} value={die.value} isHeld={die.isHeld} holdDice={() => holdDice(die.id)} />);
 
   return (
     <>
       <div className="flex justify-center items-center font-karla bg-[#0b2434] p-[20px] h-svh">
-        <div className="flex flex-col gap-[20px] justify-center items-center bg-[#f5f5f5] rounded-sm max-w-[800px] aspect-square h-[95%] p-[20px]">
+        <div className="flex flex-col gap-[20px] justify-center items-center bg-[#f5f5f5] rounded-sm max-w-[800px] aspect-square h-[80%] p-[20px]">
           <div className="flex flex-wrap justify-center items-center gap-[20px] w-[350px]">
             {diceElement}
           </div>
-          <button onClick={rollDice} className="active:shadow-[inset_5px_5px_10px_-3px_rgba(0,0,0,0.7)] font-karla text-[1.2rem] text-white bg-[#5035ff] rounded-sm py-1 px-7">Roll</button>
+          <button
+            onClick={() => setDice(allNewDice())}
+            className="active:shadow-[inset_5px_5px_10px_-3px_rgba(0,0,0,0.7)] font-karla text-[1.2rem] text-white bg-[#5035ff] rounded-sm py-1 px-7"
+          >
+            Roll
+          </button>
         </div>
       </div>
     </>
